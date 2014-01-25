54ef239acadb4e3ca0cef598d9d16923060c76f4
Cullen Jennings <fluffy@iii.ca>
2014-01-25 11:07:30 -0800

    set up basic template

---
+++ image-edit/imgEdit.js
@@ -0,0 +1,23 @@
+
+
+var Fluffy = Fluffy || {}; // setup namespace
+
+Fluffy.ImgEdit = function() // setup module 
+{
+    var inputCanvas;
+    var outputCanvas;
+
+    function init()
+    {
+    }
+
+    var publicExport =
+        {
+         
+            init: init
+        };
+
+    return publicExport;
+}();
+
+Fluffy.ImgEdit.init();
