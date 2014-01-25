55af4cab7138e13c30211afb8eb3f5fe38ed9924
sean <sean@ubuntu.(none)>
2014-01-25 13:43:11 -0700

    scrapper runs while True ... sleeps for 15

--- scraper/twitter_scraper.py
+++ scraper/twitter_scraper.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
 import os
+import time
 
 consumer_key = os.environ['consumer_key'] 
 consumer_secret = os.environ['consumer_secret']
@@ -10,31 +11,28 @@ access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
 if __name__ == '__main__':
-	auth = OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
+	while True:	
+		auth = OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
 
-	api = API(auth)
+		api = API(auth)
 
-	results = api.search(q="hackyyc", include_entities = True)
-	for result in results:
-		#print dir(result)
-		tweet_text = result.text
-		if result.geo:
-			print 'GEO not NONE'
-		entities = result.entities
-		if 'media' in entities:
-			curr_media = entities['media']
-			for media_item in curr_media:
-				if media_item['type'] == 'photo':
-					mid = media_item['id']
-					real_url = media_item['media_url']
-					twitter_url = media_item['display_url']
-					print mid
-					print real_url, twitter_url
-					print (media_item)
-					print '----'
-
-
-	
-		
-		
+		results = api.search(q="hackyyc", include_entities = True)
+		for result in results:
+			#print dir(result)
+			tweet_text = result.text
+			if result.geo:
+				print 'GEO not NONE'
+			entities = result.entities
+			if 'media' in entities:
+				curr_media = entities['media']
+				for media_item in curr_media:
+					if media_item['type'] == 'photo':
+						mid = media_item['id']
+						real_url = media_item['media_url']
+						twitter_url = media_item['display_url']
+						print mid
+						print real_url, twitter_url
+						print (media_item)
+						print '----'
+		time.sleep(15)
