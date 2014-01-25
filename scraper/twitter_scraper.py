8adffab9368a5de624528d1f65cd9572e7694f3d
sean <sean@ubuntu.(none)>
2014-01-25 14:43:00 -0700

    postgress package in requirements.txt

--- scraper/twitter_scraper.py
+++ scraper/twitter_scraper.py
@@ -10,6 +10,24 @@ consumer_secret = os.environ['consumer_secret']
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
+
+"""
+    t.string   "tweet_id"
+    t.string   "image_url"
+    t.string   "username"
+    t.datetime "created_at"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "raw"
+    t.string   "text"
+"""
+
+def process_photo(**kwargs):
+	for key, value in kwargs.iteritems():
+		print "%s : %s" % (key, value)
+	print '---'
+
+
 if __name__ == '__main__':
 	while True:	
 		auth = OAuthHandler(consumer_key, consumer_secret)
@@ -19,11 +37,15 @@ if __name__ == '__main__':
 
 		results = api.search(q="hackyyc", include_entities = True)
 		for result in results:
-			#print dir(result)
+			uid = result.author.id
+			uname = result.author.name
 			tweet_text = result.text
-			if result.geo:
-				print 'GEO not NONE'
+			tweet_id = result.id
 			entities = result.entities
+			geo = result.geo
+			coor = result.coordinates
+			source = result.source
+			created_at = result.created_at
 			if 'media' in entities:
 				curr_media = entities['media']
 				for media_item in curr_media:
@@ -31,8 +53,16 @@ if __name__ == '__main__':
 						mid = media_item['id']
 						real_url = media_item['media_url']
 						twitter_url = media_item['display_url']
-						print mid
-						print real_url, twitter_url
-						print (media_item)
-						print '----'
+						process_photo( tweet_id = tweet_id,
+							image_url = real_url,
+							username = uname,
+							text = tweet_text,
+							string = tweet_text,
+							geo = geo,
+							coor = coor,
+							source = source,
+							datetime = created_at,
+							mid = mid,
+						)
+						#print mid TODO: NEED mid
 		time.sleep(15)
