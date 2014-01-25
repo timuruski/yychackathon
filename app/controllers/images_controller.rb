ca76c2787a8230a78944ac8bdd2a631672b422c0
Tim Uruski <github@login.timuruski.name>
2014-01-25 15:32:36 -0700

    Images index doesn't error

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,18 @@
 class ImagesController < ApplicationController
-  # protect_from_forgery false
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     # just for debugging
+    render nothing: true
   end
 
   def show
     send_file Rails.root.join("public/images", "input.jpg"), type: "image/jpg", disposition: "inline"
   end
 
+  # Always accept an image, but eventually we want this to store remixed
+  # images.
   def create
-    # This always works
     render nothing: true, status: 200
   end
 
