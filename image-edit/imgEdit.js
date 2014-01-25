949b771ee4b04fc90627e941e287dcfbfeee8065
Cullen Jennings <fluffy@iii.ca>
2014-01-25 11:45:14 -0800

    can load an image

--- image-edit/imgEdit.js
+++ image-edit/imgEdit.js
@@ -3,16 +3,42 @@ var Fluffy = Fluffy || {}; // setup namespace
 
 Fluffy.ImgEdit = function() // setup module 
 {
-    var inputCanvas;
-    var outputCanvas;
+    var cavans;
+    var ctx;
 
     function init()
     {
     }
 
+    function setCanvas( c )
+    {
+	canvas = c;
+	ctx = canvas.getContext('2d');
+    }
+
+    function loadImage( url )
+    {
+	var image = new Image;
+	image.onload = function(){
+	    ctx.drawImage(image,0,0);
+	};
+	image.src = url;
+    }
+
+    function saveImage( url )
+    {
+    }
+
+    function addImage( x, y, url )
+    {
+    }
+
     var publicExport =
         {
-         
+            setCanvas: setCanvas,
+	    loadImage: loadImage,
+	    saveImage: saveImage,
+	    addImage: addImage,
             init: init
         };
 
